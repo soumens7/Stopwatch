ef0b0143413cd1d81e0fddca92fbf1d8962a62d0
Soumen sarkar <48360070+soumens7@users.noreply.github.com>
2023-09-29 10:06:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assigning the initial values of milliseconds, seconds, minutes and hour using an array
+// Assigning the initial values of milliseconds, seconds, minutes, and hour using an array
 let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
 // created variable displayTime to store value in local variable
 let displayTime = document.getElementById("displayTime");
@@ -9,28 +9,28 @@ let timer = null;
 function stopWatch() {
   milliseconds += 10; // increase milliseconds by 10 after each encounter
   if (milliseconds == 1000) {
-    //when milliseconds reaches to 1000, it will to 00 and increase the seconds by 1
+    //when milliseconds reach to 1000, it will go to 00 and increase the seconds by 1
     milliseconds = 0o0;
     seconds++;
     if (seconds == 60) {
-      //when seconds counter reaches to 60, it will change the value to 0 and increase minutes by 1
+      //when seconds counter reaches 60, it will change the value to 0 and increase minutes by 1
       seconds = 0;
       minutes++;
       if (minutes == 60) {
-        //when minutes counter reaches to 60, it will change the value to 0 and increase hours by 1
+        //when the minutes counter reaches 60, it will change the value to 0 and increase hours by 1
         minutes = 0;
         hours++;
       }
     }
   }
 
-  // if hour is less than 10 add a 0 prior to hours and store it in a local variable for furture use
+  // if hour is less than 10 add a 0 prior to hours and store it in a local variable for future use
   let h = hours < 10 ? "0" + hours : hours;
-  // if minutes is less than 10 add a 0 prior to minutes and store it in a local variable for furture use
+  // if minutes is less than 10 add a 0 prior to minutes and store it in a local variable for future use
   let m = minutes < 10 ? "0" + minutes : minutes;
-  // if seconds is less than 10 add a 0 prior to seconds and store it in a local variable for furture use
+  // if seconds is less than 10 add a 0 prior to seconds and store it in a local variable for future use
   let s = seconds < 10 ? "0" + seconds : seconds;
-  // if milliseconds is less than 10 add a 0 prior to milliseconds and store it in a local variable for furture use
+  // if milliseconds are less than 10 add a 0 prior to milliseconds and store it in a local variable for future use
   let ms =
     milliseconds < 10
       ? "00" + milliseconds
@@ -42,7 +42,7 @@ function stopWatch() {
   displayTime.innerHTML = h + ":" + m + ":" + s + "." + ms;
 }
 
-// create a function to start the StopWatch
+// created a function to start the StopWatch
 function watchStart() {
   // if any ongoing interval is present (or running) stop it and then restart it
   if (timer !== null) {
@@ -51,16 +51,16 @@ function watchStart() {
   timer = setInterval(stopWatch, 10);
 }
 
-// create a function to stop the StopWatch
+// created a function to stop the StopWatch
 function watchStop() {
-  // clear the previous interval
+  //Clear the previous interval
   clearInterval(timer);
 }
 
 // create a function to reset the StopWatch
 function watchReset() {
   clearInterval(timer);
-  // clear the previous interval and assign the default values to the timer
+  //Clear the previous interval and assign the default values to the timer
   [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
   displayTime.innerHTML = "00:00:00.000";
 }
